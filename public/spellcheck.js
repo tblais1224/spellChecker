7f403b5c04faac4a1b3c6f426780f6c0b9917240
Tom Blais <thomasblais.student@careerdevs.com>
2019-05-03 04:48:47 +0000

    fix

--- public/spellcheck.js
+++ public/spellcheck.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     $('#submit').on('click', function() {
         var userInput = document.getElementById("user-input").value;
+        //makes api call and gets response
         $.post("/", { input: userInput }, function(result){
             console.log(result)
             $("#checkedPar").remove();
